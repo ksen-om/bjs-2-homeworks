9e36f6ca25e9ca967b4c3087a54bf4db52e48c2b
ksen-om <ks.vl.om@gmail.com>
2022-08-08 12:53:49 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let student3 = new Student('Tony', 'male', 37);
+let student4 = new Student('Olga', 'female', 35);
+let student5 = new Student('Elena', 'female', 32);
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
+
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function(...marks) {
+  if(this.marks === undefined) {
+    this.marks = marks;
+  }
+  else {
+    this.marks.push(...marks);
+  }
+};
+
+Student.prototype.getAverage = function() {
+ return this.marks.reduce((acc, item) => acc + item) / this.marks.length;
 }
 
-// ваш код для остальных методов
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
