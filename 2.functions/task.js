fae4d11d97694e3cad9141f8e0aa61c53737d8fe
ksen-om <ks.vl.om@gmail.com>
2022-08-09 10:20:58 +0300

    first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,22 @@
+"use strict"
+
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+ 
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+}
+avg = parseFloat ((sum/arr.length).toFixed(2));
   // Ваш код
 
   return { min: min, max: max, avg: avg };
@@ -9,23 +24,38 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumElements = func(arrOfArr[i]);
+    if (sumElements > max) {
+      max = sumElements;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
+  let min = arr[0];
+  let max = arr [0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    } 
+  }
+  let difference = Math.abs(max - min);
+  return difference;
   // Ваш код
 }
