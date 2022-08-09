a10b2caee232b5455361594f285fe03cbbeb9582
ksen-om <ks.vl.om@gmail.com>
2022-08-09 10:25:49 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  if (arr1.length === arr2.length && arr1.every ((item, index) => item === arr2[index])) {
+    result = true;
+  }
+  else result =  false;
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
 
   // Ваш код
 
