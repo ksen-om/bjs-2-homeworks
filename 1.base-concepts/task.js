7f9d2f47c0cec6b325cd0453e77d2380ef6a3c32
ksen-om <ks.vl.om@gmail.com>
2022-08-09 10:17:33 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  let x1, x2;
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push (x1, x2);
+  }
+  else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push (x1);
+  }
+  else if  (d < 0) {
+    return arr;
+  }
   return arr; // array
 }
 
